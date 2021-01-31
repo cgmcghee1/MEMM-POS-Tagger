a95017d09f9928bc1c6e2b88e32e2c726516319e
Charlie Mcghee <charliemcghee@Charlies-MacBook-Pro.local>
2021-01-31 12:27:34 +0000

    commenting code

--- Functions.py
+++ Functions.py
@@ -4,33 +4,47 @@ import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 
 def softmax(z):
-    
+
     """
     Softmax function which takes an input z and produces a probability
     distribution for each of the elements. The maximum of z is removed from
     each element to help with numerical stability as the exponent can make z
     large quickly.
+
+    :param z: input numpy matrix
+    :return softmax distribution over input z
     """
-    
+
     z -= np.max(z)
     return np.exp(z)/np.sum(np.exp(z), axis = 0)
 
 def loss(y, yhat):
-    
+
     """
     Calculates the loss for a prediction versus the actual value.
+
+    :param y: target tag vector
+    :param yhat: estimated tag vector
+    :return loss
     """
-    
+
     return -(1/y.shape[1])*np.sum(y*np.log(yhat))
 
 def grad(x, y, yhat, lambd, w):
-    
+
     """
     Calculates the gradient for gradient descent based on the feature input X,
     the prediction y and the actual value y. The extra term lambd * w is a form of
     regularisation which helps prevent overfitting.
+
+    :param x: vectorised input variable
+    :param y: target tag vector
+    :param yhat: estimated tag vector
+    :param lambd: regularisation paramter
+    :param w: numpy matrix of weights
+    :return grad: gradient matrix
     """
-    
+
     cost = np.subtract(y,yhat)
     grad = -(1/y.shape[1])*((np.dot(x, cost.T)) + lambd * w)
     return(grad)
@@ -38,11 +52,15 @@ def grad(x, y, yhat, lambd, w):
 
 
 def onehot(y):
-    
+
     """
     Takes an input y and produces a one hot encoding for that input.
+
+    :param y: input list of tags
+    :return y: output onehot representation of input tags
+    :return tagset: list of unique tags
     """
-    
+
     onehotframe = pd.get_dummies(pd.DataFrame(y))
     tagset = [tag[2:] for tag in list(onehotframe.columns)]
     y = onehotframe.to_numpy(dtype = 'float64')
@@ -50,12 +68,17 @@ def onehot(y):
 
 
 def dictfeatures(sentence, word, index):
-    
+
     """
     For any token input in a given sequence, this function returns a dictionary
     of the features for that token.
+
+    :param sentence: tagged sentence from NLTK corpus
+    :param word: string current word input
+    :param index: integer representing position of word in sentence
+    :return featuredict: feature dictionary for input word
     """
-    
+
     if index == 0:
         prevtag = 'START'
     else:
--- MEMM.py
+++ MEMM.py
@@ -7,18 +7,38 @@ from sklearn.feature_extraction import DictVectorizer
 from Functions import *
 
 class Model:
-    
+
     """
-    This is a POS tagger based on a Maximum Entropy Markov Model. The default
-    training corpus is the treebank corpus and the hyperparameters for regression
-    are tuned to this corpus.
+    This is a POS tagger based on a Maximum Entropy Markov Model which can be
+    found in Jurafsky and Martin's 'Speech and Language Processing', the draft third edition
+    can be found here:
+
+    https://web.stanford.edu/~jurafsky/slp3/ed3book.pdf
+
+    The default training corpus is the NLTK treebank corpus and the hyperparameters
+    for regression are tuned to this corpus.
     """
-    
+
     def __init__(self, corpus = treebank, ttsplit = 0.9):
-        
+
         """
         Initialises the train and test data for the model, utilising the DicVectorizer
         tool from SciKit Learn and a onhot encoding function which uses Pandas.
+
+        :param corpus: NLTK corpus
+        :param ttsplit: float representing the split between size of train and test set
+        :classattribute X_dict: list of input variables, represented as feature dictionaries
+        :classattribute y: list representing target variables
+        :classattribute vectorizer: vectorizer function
+        :classattribute X: vectorized input variables
+        :classattribute tagset: list of unique tags
+        :classattribute X_train: training set of input variables
+        :classattribute X_test: test set of input variables
+        :classattribute y_train: training set of target variables
+        :classattribute y_test: test set of target variables
+        :classattribute numclasses: number of possible tags
+        :classattribute numfeatures: number of features
+        :classattribute w: weight matrix
         """
 
         self.X_dict = []
@@ -47,11 +67,15 @@ class Model:
 
 
     def evaluate(self):
-        
+
         """
         Predicts the tag for words in the test data and returns a percentage accuracy
+
+        :classattribute X_test: test set of input variables
+        :classattribute w: weight matrix
+        :classattribute y_test: test set of target variables
         """
-        
+
         correct = 0
         for i in range(self.X_test.shape[1]):
             yhat = softmax(np.dot(self.w.T ,self.X_test[:, i]))
@@ -63,12 +87,19 @@ class Model:
         return None
 
     def train(self, batch_size = 32 , lrate = 0.6, lambd = 0.05):
-        
+
         """
         Trains the weights via multinomial logistic regression and stores them
         in a class variable self.w
+
+        :param batch_size: integer representing the batch size for minibatch processing
+        :param lrate: float representing the learning rate
+        :param lambd: float representing lambda value for regularisation
+        :classattribute X_train: training set of input variables
+        :classattribute y_train: training set of target variables
+        :classattribute w: weight matrix
         """
-        
+
         num_cycles = int(self.X_train.shape[1]/batch_size)
         for i in range(num_cycles):
             X = self.X_train[:, (i)*batch_size:(i+1)*batch_size]
@@ -82,14 +113,20 @@ class Model:
 
 
     def tagger(self, sentence):
-        
+
         """
         Takes an input sentence and tokenises it, extracts features then vectorises them.
         A prediction is made on each token, which is then stored in a backpointer, once the
         end of the sentence is reached this backpointer is returned as the tag sequence for
         the sentence
+
+        :param sentence: input string as sentence
+        :classattribute vectorizer: vectorizer function
+        :classattribute tagset: set of tags
+        :classattribute w: weight matrix
+        :return backpointer: list of predicted tags
         """
-        
+
         backpointer = []
         splitsent = [[word,index] for (index, word) in enumerate(word_tokenize(sentence))]
         for word, index in splitsent:
